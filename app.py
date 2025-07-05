d12a895dae19962884d94343db9cacc53a7e8e6d
Dlomta08 <davitlomtadze@mziuri.ge>
2025-07-05 15:23:25 +0400

    gg

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-
+app.secret_key = "super_secret_key_123"
 
 
 # PostgreSQL connection string
